ee546cdeb5905df75fffeaf2bce30efd985dccfe
Anca Gutu <anncagutu@gmail.com>
2022-02-26 20:32:57 +0200

    .

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,15 +1,22 @@
 import './Sidebar.css';
-
+import { Input } from '@material-ui/core';
+import { SearchOutlined } from '@material-ui/icons';
+import UserHeader from '../Users/UserHeader';
 function Sidebar() {
     return (
         <div className="Sidebar">
-            <div className='sidebarHeader'>
+           
+           <UserHeader userType={'profileUser'}/>
 
-            </div>
             <div className='sidebarSearch'>
+                <div className='searchContainer'>
+                    <Input className='input' placeholder='Search or start a new chat' variant="standard" />
+                    <SearchOutlined />
+                </div>
             </div>
             <div className='sidebarChats'>
-
+                <h2 className="h2">Add new chat</h2>
+                <UserHeader userType="chatUser"/>
             </div>
         </div >
     );
