0b0d20d36f18c1c4ea82ab739ad77121d57c2db5
Anca Gutu <anncagutu@gmail.com>
2022-02-28 19:09:12 +0200

    /

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -2,31 +2,64 @@ import './Chat.css';
 import React, { useEffect, useState } from "react";
 import { Avatar, IconButton, Input } from '@material-ui/core';
 import { AttachFile, EmojiEmotions, Mic, MoreVert, SearchOutlined, Send } from '@material-ui/icons';
-
-function Chat(newChatName) {
+import { useForm } from "react-hook-form";
+function Chat() {
   const [seed, setSeed] = useState("");
   const [input, setInput] = useState("");
-  const [sentMessage, setSentMessage] = useState("");
-  const [receivedMessage, setReceivedMessage] = useState("");
-  const [isEmojiIconClicked, setIsEmojiIconClicked] = useState(false);
   const [isEmojiIconVisible, setIsEmojiIconVisible] = useState(true);
+  const [isEmpty, setIsEmpty] = useState(true);
+  const { register, handleSubmit } = useForm();
+  const [data, setData] = useState("");
+  const [sentMsg, setSentMsg] = useState("");
+  const [isReceivingVisible, setIsReceivingVisible] = useState(false);
 
-  const messages = [{ sentMessage: null, receivedMessage: null, senderName: 'Me', receiverName: 'Jessica Mirutu', chatName:newChatName }];
+  const [messages, setMessages] = useState([]);
   useEffect(() => {
 
     setSeed(Math.floor(Math.random() * 5000));
   }, [])
 
-  const onSendMessage = (event) => {
+  function onSendMessage() {
+    setIsEmpty(false);
 
-    messages.push({ sentMessage: input, receivedMessage: (input + "❤️") });
-    console.log(messages)
   }
+  //useEffect(() => {
 
-  const onEmojiIconClick = () => {
-    setIsEmojiIconClicked(true);
-    isEmojiIconVisible(true);
-  };
+  //localStorage.setItem(messages, sentMsg);
+  //let k = localStorage.getItem(messages);
+  //console.log(k, typeof k)
+  //})
+
+  useEffect(() => {
+    messages.push({ data });
+    console.log(messages);
+    for (var i = 0; i < messages.length; i++) {
+      var k = messages[i];
+      if (k) {
+        var d = k["data"];
+        if (d) {
+          var sM = d?.split('{"sentMessage":"');
+          if (sM) {
+            var sM2 = sM[1]?.split('"}');
+            if (sM2) {
+              var sM3 = sM2[0];
+              if (sM3)
+              setSentMsg(sM3);
+            }
+          }
+        }
+      }
+    }
+
+  })
+
+  const onSubmitClick = () => {
+
+    setTimeout(() => {
+      setIsReceivingVisible(true);
+    }, 3000);
+    setIsEmpty(false);
+  }
   return (
 
     <div className="Chat">
@@ -53,51 +86,57 @@ function Chat(newChatName) {
         </div>
       </div>
 
+
+
       <div className='chatBody'>
 
-        {(messages.length>1)?  (
-          <div>
-            <div className="chatReveicer">
-              <span className="chatName">{messages.senderName}</span>
-              <p>{messages.sentMessage}</p>
-              <span className="chatTimestap">3:52 pm</span>
+        {(!isEmpty) &&
 
-            </div>
+          (<div className="chatReveicer">
+            <span className="chatName">Me</span>
+            <p>{sentMsg}</p> </div>)}
 
-            <div className='chatMessage'>
-              <span className="chatName">{messages.receiverName}</span>
 
-              <p>{messages.receivedMessage}</p>
+        {(isReceivingVisible) &&
 
-              <span className="chatTimestap">6:52 pm</span>
-            </div></div>) : (<h2 className='startNewChat'>Start a new chat</h2>)}
+          (<div className="chatMessage">
+            <span className="chatName" >Jessica</span>
+            <p>{sentMsg} 💗 </p> </div>)}
+        {/* (sentMessages?.map((m) => <div className="chatReveicer">
+          <span className="chatName">Me {timeOfToday}</span>
+          <p>{m.sentMessage}</p> </div>))} */}
 
-        {/* (message.name === user.displayName) */}
-        {/* {chosenEmoji ? (
-        <span>You chose: {chosenEmoji.emoji}</span>
-      ) : (
-        <span>No emoji Chosen</span>
-      )} */}
+
+        {/* {(!isEmpty)&&(messageWasSent) && (receivedMessages?.map((m) => <div className='chatMessage' style={{ marginTop: "60px" }} >
+          <span className="chatName">Jessica Mirutu {timeOfToday}</span>
+          <p>{m.receivedMessage}</p> </div>))} */}
+
+
+        {(isEmpty) && (<h2 className='startNewChat'>Start a new chat</h2>)}
       </div>
+
+
+
 
       <div className='chatBottom'>
         <div className='chatSendContainer'>
           <IconButton>
-            <EmojiEmotions className='iconsColor' onClick={onEmojiIconClick} />
-            {/* {isEmojiIconClicked ? (<Emoji isEmojiIconVisible />) : (<EmojiEmotions className='iconsColor' onClick={onEmojiIconClick} />)} */}
+            <EmojiEmotions className='iconsColor' />
           </IconButton>
 
-
-          <Input className='input' placeholder='Type a message ...' value={input} onChange={e => setInput(e.target.value)} variant="standard" />
 
           <IconButton>
             <Mic className='iconsColor' />
           </IconButton>
 
 
-          <IconButton>
-            <Send className='iconsColor' type="submit" onClick={onSendMessage} />
-          </IconButton>
+          <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))} className="form">
+
+            <Input className='input' placeholder='Type a message ...' {...register("sentMessage")}
+
+            />
+           <input type="submit" onClick={onSubmitClick} class="submitButton"/>
+          </form>
         </div>
       </div>
 
